04ee0e13c0f80bb4716920bc217a8f341e4f3f0b
GinaSoraya <ginasry08@gmail.com>
2024-04-15 01:59:15 +0700

    menyelesaikan program dengan runtime configuration

--- tpmodul8_1302223070/Program.cs
+++ tpmodul8_1302223070/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Transactions;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+        UICovidConfig uiConfig = new UICovidConfig();
+
+        uiConfig.ReadConfigFile();
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {uiConfig.config.satuan_suhu}");
+        double suhu = Convert.ToDouble( Console.ReadLine() );
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hari = Convert.ToInt32( Console.ReadLine() );
+
+        if (uiConfig.config.satuan_suhu == "Celcius")
+        {
+            if (suhu >= 36.5 && suhu <= 37.5 && hari < uiConfig.config.batas_hari_demam)
+            {
+                Console.WriteLine(uiConfig.config.pesan_diterima);
+            } 
+            else
+            {
+                Console.WriteLine(uiConfig.config.pesan_ditolak);
+            }
+        }
+        else if (uiConfig.config.satuan_suhu == "Fahrenheit")
+        {
+            if (suhu >= 97.7 && suhu <= 99.5 && hari < uiConfig.config.batas_hari_demam)
+            {
+                Console.WriteLine(uiConfig.config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(uiConfig.config.pesan_ditolak);
+            }
+        }
+
+        config.UbahSatuan();
+        uiConfig.ReadConfigFile();
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {uiConfig.config.satuan_suhu}");
+        suhu = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        hari = Convert.ToInt32(Console.ReadLine());
+
+        if (uiConfig.config.satuan_suhu == "Celcius")
+        {
+            if (suhu >= 36.5 && suhu <= 37.5 && hari < uiConfig.config.batas_hari_demam)
+            {
+                Console.WriteLine(uiConfig.config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(uiConfig.config.pesan_ditolak);
+            }
+        }
+        else if (uiConfig.config.satuan_suhu == "Fahrenheit")
+        {
+            if (suhu >= 97.7 && suhu <= 99.5 && hari < uiConfig.config.batas_hari_demam)
+            {
+                Console.WriteLine(uiConfig.config.pesan_diterima);
+            }
+            else
+            {
+                Console.WriteLine(uiConfig.config.pesan_ditolak);
+            }
+        }
+    }
+}
---
+++ tpmodul8_1302223070/UICovidConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+public class UICovidConfig
+{
+    public CovidConfig config;
+    public const string filePath = "C:\\GINA\\KULIAH\\SEMESTER 4\\KONTRUKSI PERANGKAT LUNAK\\PRAKTIKUM\\MINGGU 8\\tpmodul8_1302223070\\tpmodul8_1302223070\\CovidConfig.json";
+    public UICovidConfig()
+    {
+        try
+        {
+            ReadConfigFile();
+        }
+        catch (Exception)
+        {
+            setDefault();
+            WriteNewConfigFile();
+        }
+    }
+    public CovidConfig ReadConfigFile()
+    {
+        string jsonData = File.ReadAllText(filePath);
+        config = JsonSerializer.Deserialize<CovidConfig>(jsonData);
+        return config;
+    }
+    public void setDefault()
+    {
+        config.satuan_suhu = "Celcius";
+        config.batas_hari_demam = 14;
+        config.pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        config.pesan_diterima = "Anda dipersilahkan masuk ke dalam gedung ini";
+    }
+    public void WriteNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            WriteIndented = true,
+        };
+        string jsonString = JsonSerializer.Serialize(config, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+}
